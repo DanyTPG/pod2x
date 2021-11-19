4b557de54a4785b1f6a599f51908bd235fd3045f
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2021-11-19 18:05:33 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,41 +4,7 @@
 
 rm -rf /etc/xray/config.json
 cat << EOF > /etc/xray/config.json
-{
-  "inbounds": [
-    {
-      "port": $PORT,
-      "protocol": "vless",
-      "settings": {
-        "decryption": "none",
-        "clients": [
-          {
-            "id": "$UUID"
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws"
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "shadowsocks",
-                  "settings": {
-                        "servers": [
-                                {
-                                "address": "$server",
-                                "method": "$enc",
-                                "ota": false,
-                                "port": $server_port,
-                                "password": "$pass"
-                                }
-                        ]
-            }
-    }
-  ]
-}
+$cn
 EOF
 
 #run xray
